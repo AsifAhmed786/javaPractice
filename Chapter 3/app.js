2294211c7fc9b46364dbd17e1ff3e3966e6a3d83
asifahmed786 <asifahmed1us1@gmail.com>
2020-06-06 16:12:06 +0500

    chapter 3 completed

---
+++ Chapter 3/app.js
@@ -0,0 +1,21 @@
+// Task 1
+var age = 40;
+alert("I am "+age+" years old")
+
+
+// Task 2
+var count = 25;
+alert("You have visited this site "+count+" times")
+
+// Task 3
+var birthYear = 1980;
+document.write("My birth year is "+birthYear+"<br>")
+document.write("Data type of my declared variable is number")
+
+
+// Task 4
+var visitorName = "John";
+var product = "T-Shirt";
+var qty = 5;
+document.write("<br><br><br>")
+document.write(visitorName+"<b> Does</b> ordered <b>"+qty+" "+product+"</b>(s) on XYZ Clothing Store")

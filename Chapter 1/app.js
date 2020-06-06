a2ed9932927c7bc529bfed68856b8d32272a3a81
asifahmed786 <asifahmed1us1@gmail.com>
2020-06-06 16:02:26 +0500

    Assignment 2 completed

---
+++ Chapter 1/app.js
@@ -0,0 +1,16 @@
+//chapter 1
+//Task 1
+alert("Welcome to our website");
+
+// Task 2
+alert("Error! Please enter a valid password");
+
+// Task 3
+alert("Welcome to JS Land\nHappy Coding!")
+
+
+// Task 4
+alert("Welcome to JS Land")
+alert("Happy Coding!")
+
+
---
+++ Chapter 2/app.js
@@ -0,0 +1,34 @@
+// Task 1
+var username = 5;
+
+// Task 2
+var myName = "Asif Ahmed";
+
+// Task 3
+var message = "Hellow World";
+
+// Task 4
+var age = 30;
+var education = "Certified Mobile Application Developer";
+alert("Name: "+myName);
+alert("Age: "+age);
+alert("Education: "+education);
+
+
+// Task 5
+var piz = "PIZZA"
+alert(piz+"\n"+piz.slice(0,4)+"\n"+piz.slice(0,3)+"\n"+piz.slice(0,2)+"\n"+piz.slice(0,1))
+
+
+// Task 6
+var email = "example@example.com"
+alert("My email address is "+email)
+
+
+// Task 7
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn from the Book "+book)
+
+
+// Task 8
+document.write("Yah! I can write HTML content through JavaScript")
